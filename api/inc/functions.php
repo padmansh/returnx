524afb3056ab446a4eb58110c1a3d77e417f072a
Aniket Padmanabh <pudmaansh@gmail.com>
2024-09-22 12:43:50 +0530

    url

--- api/inc/functions.php
+++ api/inc/functions.php
@@ -62,3 +62,11 @@ function shopify_call($token, $shop, $api_endpoint, $query = array(), $method = 
 	}
     
 }
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
--- api/index.php
+++ api/index.php
@@ -1,13 +1,20 @@
 <?php 
 
-// Get 'shop' parameter from URL
-$shop = $_GET['shop'];
+require_once("inc/functions.php");
 
-// Remove '.myshopify.com' (14 chars)
-$cutUrl = substr($shop, 0, -14);
+// // Get 'shop' parameter from URL
+// $shop = $_GET['shop'];
+
+// // Remove '.myshopify.com' (14 chars)
+// $cutUrl = substr($shop, 0, -14);
+
+$shop = isset($_GET['shop']) ? $_GET['shop'] : '';
+
+// Remove '.myshopify.com' from the end of the shop parameter
+$shop = preg_replace('/\.myshopify\.com$/', '', $shop);
 
 // Base URL for redirection
-$redirectUrl = "https://returnx.vercel.app/api/install.php?shop=" . urlencode($cutUrl);
+$redirectUrl = "https://returnx.vercel.app/api/install.php?shop=" . urlencode($shop);
 
 // Check if 'rxref' parameter is set and append it if it exists
 if (isset($_GET['rxref'])) {
@@ -17,4 +24,6 @@ if (isset($_GET['rxref'])) {
 
 // Redirect to install.php with the appropriate parameters
 header("Location: " . $redirectUrl);
+
+exit();
 ?>
